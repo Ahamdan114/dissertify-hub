6179b44f168ce631bcc8aa03be11e00b98fc2029
Ahmed-Iulian Hamdan <hamdan.ahmed002@gmail.com>
2024-01-29 17:43:46 +0200

    Added authentication component for future implementation

---
+++ client/src/components/Authentication.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Authentication.css";
+
+import { useNavigate } from "react-router-dom";
+
+const Authentication = () => {
+    return <>
+        
+    </>
+};
+
+export default Authentication;
