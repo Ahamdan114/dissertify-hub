0372ab89566870e883e95eb23547e315d578d071
Ahmed-Iulian Hamdan <hamdan.ahmed002@gmail.com>
2024-01-29 18:09:50 +0200

    Added initial functionalities

--- client/src/components/Authentication.jsx
+++ client/src/components/Authentication.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import "./Authentication.css";
-
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+import "./Authentication.css";
 
 const Authentication = () => {
     const navigate = useNavigate();
+    const [isVisible, setIsVisible] = useState(false);
+    const authenticationSubmit = (e) => {
+        e.preventDefault();
+        // navigate("/home");
+    };
     return (
         <>
             <div className="auth-container">
@@ -16,16 +22,54 @@ const Authentication = () => {
                         <form onSubmit={(e) => authenticationSubmit(e)}>
                             <div className="auth-form-field">
                                 <label htmlFor="email">Email</label>
-                                <input type="email" id="email" name="email" />
+                                <input
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    placeholder="Enter email..."
+                                />
                             </div>
                             <div className="auth-form-field">
                                 <label htmlFor="password">Password</label>
-                                <input type="password" id="password" name="password" />
+                                <input
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    placeholder="Enter password..."
+                                />
                             </div>
                             <div className="auth-form-field">
-                                <button type="submit" onClick={(e) => authenticationSubmit(e)}>Sign in</button>
+                                <label htmlFor="password">Confirm password</label>
+                                <input
+                                    type={`${isVisible ? "text" : "password"}`}
+                                    id="password"
+                                    name="password"
+                                    placeholder="Enter password..."
+                                />
+                                <button onClick={() => setIsVisible(!isVisible)}>
+                                    Make it visible
+                                </button>
                             </div>
                         </form>
+                        <div className="auth-form-button">
+                            <button
+                                type="submit"
+                                onClick={(e) => authenticationSubmit(e)}
+                            >
+                                Sign in
+                            </button>
+                        </div>
+                        <div className="auth-form-login">
+                            <p className="auth-form-login-description">
+                                Already have an account? Login
+                            </p>
+                            <button
+                                type="submit"
+                                onClick={(e) => authenticationSubmit(e)}
+                            >
+                                Login
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
